e1d590f5e0a93db1d4a0016cc28f86a314705ab3
PauloGabriel01 <paulogabriel042@gmail.com>
2024-09-12 00:46:03 -0400

    file name and content changed

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,28 @@
+// Classe do arquivo
+
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        
+        
+        
+        // Forma de declarar um valor decimal em Java
+
+        double minimumWage = 2500;
+        
+        // Casting: Alterando o tipo de variável
+
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short shortNumber2 = (short) normalNumber;
+        
+        int number = 5;
+
+        number = 10;
+
+        System.out.println(number);
+
+        // Constantes: Valores que não sofrem alterações
+        final double PI_VALUE = 3.14;
+       
+    }
+}
