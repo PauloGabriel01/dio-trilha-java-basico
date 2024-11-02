0eb8a3d91846aea0161c6924041a936b0c6eac52
Paulo Gabriel <paulogabriel042@gmail.com>
2024-11-02 11:56:02 -0400

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -6,13 +6,8 @@ public class ContaTerminal {
         // TODO: "Conhecer e importar a classe Scanner"
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        /* 
-         * int numero_conta
-         * String agencia
-         * 
-         * double saldo
-         */
-        // Exibir as mensagens para o nosso usuário
+
+        // Inserir valores no terminal pela classe Scanner
         System.out.println("Conta Bancária");
         System.out.println("Nome do Cliente: ");
         String nome_cliente = scanner.next();
@@ -25,8 +20,6 @@ public class ContaTerminal {
         
         System.out.println("Saldo: ");
         double saldo = scanner.nextDouble();
-
-        // Obter pela scanner os valores digitados no terminal
 
         // Exibir a mensagem conta criada
         System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.\n", nome_cliente, agencia, numero_conta, saldo);
